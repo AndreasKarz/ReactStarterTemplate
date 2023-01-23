31780269328e3b827e106ac2d7c7e74780b43c4f
Andreas Karz <andreas.karz@outlook.com>
2023-01-23 18:12:37 +0100

    Expand .env to HTML

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -6,14 +6,13 @@ function Services() {
 	useDocumentTitle(`${import.meta.env.VITE_WEBSITE_NAME}${location}`);
 
 	let openingHours = JSON.parse(import.meta.env.VITE_OPENING_HOURS);
-	let address = JSON.parse(import.meta.env.VITE_ADDRESS);
 
 	return (
 		<>
 			<h1>Services</h1>
 			<div>{import.meta.env.VITE_OPENING_HOURS}</div>
 			<p>{openingHours[0]['dayOfWeek']}</p>
-			<p>{address['NAME']}</p>
+			<p>{import.meta.env.VITE_ADDRESS_STREET}</p>
 		</>
 	);
 }
